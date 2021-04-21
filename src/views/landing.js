9d7a7b5ff2e37f03ccb1b8cb5dc86939cbc917da
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2021-04-21 02:17:09 -0500

    Add landing page footer

--- src/views/landing.js
+++ src/views/landing.js
@@ -1,11 +1,20 @@
 
-import { div } from '../lib/vnodes/html'
+import { a, div, text } from '../lib/vnodes/html'
 import Link from './_link'
 
-const Landing = () => {
+const Landing = (state, dispatch) => {
   return div({ class: 'landing' }, [
     div({ class: 'landing-graphic' }, [
       Link({ to: '/overview' }, 'Continue to Insights')
+    ]),
+    div({ class: 'landing-footer' }, [
+      text('© Dustin Dowell, ' + state.footer.year + '\n'),
+      a({ href: '/legal' }, [
+        text('Terms of Service')
+      ]),
+      a({ href: '/legal' }, [
+        text('Privacy Policy')
+      ])
     ])
   ])
 }
