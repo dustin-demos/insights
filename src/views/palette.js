83194b984dc498327d52d6664aea8ac99965a42a
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2021-06-11 06:29:52 -0500

    Start using CSS vars

---
+++ src/views/palette.js
@@ -0,0 +1,24 @@
+
+export default {
+  view: () => {
+    return (
+      <div class='palette'>
+        <div>
+          <div class='-red'></div>
+          <div class='-orange'></div>
+          <div class='-yellow'></div>
+          <div class='-green'></div>
+          <div class='-blue'></div>
+          <div class='-purple'></div>
+        </div>
+        <div>
+          <div class='-red'></div>
+          <div class='-yellow'></div>
+          <div class='-green'></div>
+          <div class='-blue'></div>
+          <div class='-purple'></div>
+        </div>
+      </div>
+    )
+  }
+}
